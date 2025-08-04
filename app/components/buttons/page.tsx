796a73ad5a4b29488e61f8729ab838cd54f5c822
akiran1sei <nakamori.1224@gmail.com>
2025-08-04 12:48:09 +0900

    mainButton作成

--- app/components/buttons/page.tsx
+++ app/components/buttons/page.tsx
@@ -54,42 +54,90 @@ export const LinkIconButton: React.FC<IconButtonType> = ({ value }) => {
 interface MainButtonType {
   sizeValue: string;
   textValue: string;
+  buttonColor: string;
 }
+// --color-danger → .btn-danger: 「削除」などの危険な操作に。
+// --color-main → .btn-main: 「PDFをダウンロード」など、最も主要なアクションに。
+// --color-warning → .btn-warning: 「編集」など、注意を促すアクションに。
+// --color-success → .btn-success: 「保存」など、成功を表すアクションに。
+// --color-secondary → .btn-secondary: 「リセット」「検索」「並び替え」など、控えめなサブアクションに。
+//  例： <MainButton sizeValue="large" textValue="編集" buttonColor="btn-warning"/>
 export const MainButton: React.FC<MainButtonType> = ({
   sizeValue,
   textValue,
+  buttonColor,
 }) => {
   if (sizeValue === "large") {
     return (
-      <button type="button">
-        <span className="buttonText">{textValue}</span>
+      <button
+        type="button"
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.largeButton} ${styles[buttonColor]}`}
+      >
+        <span className={styles.buttonText}>{textValue}</span>
       </button>
     );
   } else if (sizeValue === "middle") {
-  } else if (sizeValue === "small") return <button>{textValue}</button>;
+    return (
+      <button
+        type="button"
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.middleButton} ${styles[buttonColor]}`}
+      >
+        <span className={styles.buttonText}>{textValue}</span>
+      </button>
+    );
+  } else if (sizeValue === "small") {
+    return (
+      <button
+        type="button"
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.smallButton} ${styles[buttonColor]}`}
+      >
+        <span className={styles.buttonText}>{textValue}</span>
+      </button>
+    );
+  }
 };
 
 interface MainLinkButtonType {
   sizeValue: string;
   textValue: string;
   urlValue: string;
+  buttonColor: string;
 }
 export const MainLinkButton: React.FC<MainLinkButtonType> = ({
   sizeValue,
   textValue,
   urlValue,
+  buttonColor,
 }) => {
   if (sizeValue === "large") {
-    <Link href={`${urlValue}`} passHref>
-      <a className="updateButton">
-        <span className="buttonText">{textValue}</span>
-      </a>
-    </Link>;
+    return (
+      <Link
+        href={`${urlValue}`}
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.largeButton} ${styles[buttonColor]}`}
+        passHref
+      >
+        <span className={styles.buttonText}>{textValue}</span>
+      </Link>
+    );
   } else if (sizeValue === "middle") {
-  } else if (sizeValue === "small") return <button>{textValue}</button>;
+    return (
+      <Link
+        href={`${urlValue}`}
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.middleButton} ${styles[buttonColor]}`}
+        passHref
+      >
+        <span className={styles.buttonText}>{textValue}</span>
+      </Link>
+    );
+  } else if (sizeValue === "small") {
+    return (
+      <Link
+        href={`${urlValue}`}
+        className={`${styles.mainButton} ${styles.buttonShadow} ${styles.smallButton} ${styles[buttonColor]}`}
+        passHref
+      >
+        <span className={styles.buttonText}>{textValue}</span>
+      </Link>
+    );
+  }
 };
-
-export const SearchButton = () => {};
-export const SortButton = () => {};
-export const SubmitButton = () => {};
-export const DeleteButton = () => {};
--- app/pages/list/page.tsx
+++ app/pages/list/page.tsx
@@ -1,14 +1,65 @@
-import { MainButton } from "@/app/components/buttons/page";
+import { MainButton, MainLinkButton } from "@/app/components/buttons/page";
 
 const ListPage = () => {
+  const value = `/list`;
   return (
     <div>
       <h1>List Page</h1>
       <p>This is the list page content.</p>
       {/* 他のコンテンツやコンポーネントをここに追加 */}
-      <MainButton sizeValue="large" textValue="ラージボタン" />
-      <MainButton sizeValue="large" textValue="ラージボタン" />
-      <MainButton sizeValue="large" textValue="ラージボタン" />
+      <MainButton sizeValue="large" textValue="削除" buttonColor="btn-danger" />
+      <MainButton
+        sizeValue="middle"
+        textValue="削除"
+        buttonColor="btn-danger"
+      />
+      <MainButton
+        sizeValue="large"
+        textValue="PDFをダウンロード"
+        buttonColor="btn-main"
+      />
+      <MainButton
+        sizeValue="large"
+        textValue="編集"
+        buttonColor="btn-warning"
+      />
+      <MainButton
+        sizeValue="large"
+        textValue="保存"
+        buttonColor="btn-success"
+      />
+      <MainButton
+        sizeValue="middle"
+        textValue="リセット"
+        buttonColor="btn-secondary"
+      />
+      <MainButton
+        sizeValue="middle"
+        textValue="セルフVer"
+        buttonColor="btn-success"
+      />
+      <MainButton
+        sizeValue="middle"
+        textValue="お店Ver"
+        buttonColor="btn-success"
+      />
+      <MainButton
+        sizeValue="small"
+        textValue="並び替え"
+        buttonColor="btn-secondary"
+      />
+      <MainButton
+        sizeValue="small"
+        textValue="検索"
+        buttonColor="btn-secondary"
+      />
+
+      <MainLinkButton
+        sizeValue="large"
+        textValue="編集"
+        buttonColor="btn-warning"
+        urlValue={`/pages/${value}`}
+      />
     </div>
   );
 };
