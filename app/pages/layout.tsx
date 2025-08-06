85b7f5cf952fa017419906e50374904d64905d0f
akiran1sei <nakamori.1224@gmail.com>
2025-08-06 16:46:20 +0900

    UpperButtonコンポーネントファイルの設置と削除

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
 import HeaderComponent from "../components/header/page";
-
+import { UpperButton } from "@/app/components/buttons/upper/page";
 export default function PagesLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <HeaderComponent />
+      <UpperButton />
       {children}
     </>
   );
