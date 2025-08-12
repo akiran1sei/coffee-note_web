608b48570f20259dde98f444007ae35a230312e7
akiran1sei <nakamori.1224@gmail.com>
2025-08-12 16:45:05 +0900

    作成したカテゴリーコンポーネントらをインポート

--- app/pages/create/page.tsx
+++ app/pages/create/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import styles from "@/app/styles/Pages.module.css";
-import {
-  InputComponent,
-  NumberComponent,
-} from "@/app/components/form/InputComponent/page";
-import { RangeComponent } from "@/app/components/form/RangeComponent/page";
-import { translationMap } from "@/app/utils/translations";
+
 import { TastingEvaluationComponent } from "@/app/components/itemCard/TastingEvaluation/page";
-import {
-  ConditionalMeasurementSelector,
-  CoffeeProcessingSelect,
-  CoffeeTypesSelect,
-  HierarchicalCoffeeSelect,
-  MinuteSecondComponent,
-  HourComponent,
-} from "@/app/components/form/SelectComponent/page";
-import { TextAreaComponent } from "@/app/components/form/TextAreaComponent/page";
+
+import { CoffeeBeansComponent } from "@/app/components/itemCard/CoffeeBean/page";
+import { BrewingRecipeComponent } from "@/app/components/itemCard/BrewingRecipe/page";
+import { MemoAreaComponent } from "@/app/components/itemCard/MemoArea/page";
 
 const CreatePage = () => {
-  const [extractionMethod, setExtractionMethod] = useState("");
-  const [equipment, setEquipment] = useState("");
-  const [measurement, setMeasurement] = useState("");
-
   const [formValue, setFormValue] = useState({
+    imageUrl: "",
     coffeeName: "",
     productionArea: "",
     variety: "選択していません。",
@@ -49,119 +36,10 @@ const CreatePage = () => {
   return (
     <div className={styles.createPageContents}>
       <h1>Create Page</h1>
-      <InputComponent
-        dataTitle="コーヒー名"
-        value={formValue.coffeeName}
-        onChange={(value: string) => {
-          setFormValue({ ...formValue, coffeeName: value });
-        }}
-        labelText="coffeeName"
-      />
-      <InputComponent
-        dataTitle="産地"
-        value={formValue.productionArea}
-        onChange={(value: string) => {
-          setFormValue({ ...formValue, productionArea: value });
-        }}
-        labelText="productionArea"
-      />
-      <CoffeeProcessingSelect
-        dataTitle="焙煎度"
-        value={formValue.roastingDegree}
-        onChange={(value: string) => {
-          setFormValue({ ...formValue, roastingDegree: value });
-        }}
-        labelText="roastingDegree"
-      />
-      <CoffeeTypesSelect
-        dataTitle="品種"
-        onChange={(value: string) => {
-          setFormValue({ ...formValue, variety: value });
-        }}
-        value={formValue.variety}
-        labelText="variety"
-      />
-      {/* まず、抽出方法を選択するコンポーネントを配置 */}
-      <HierarchicalCoffeeSelect
-        primaryTitle="抽出方法"
-        secondaryTitle="抽出器具"
-        primaryValue={extractionMethod}
-        secondaryValue={equipment}
-        onPrimaryChange={setExtractionMethod}
-        onSecondaryChange={setEquipment}
-        labelText="extractionMethod"
-      />
-      {/* 抽出方法が選択された場合にのみ、量り方のコンポーネントを表示 */}
-      {extractionMethod && (
-        <ConditionalMeasurementSelector
-          dataTitle="量り方"
-          value={measurement}
-          onChange={setMeasurement}
-          extractionMethod={extractionMethod}
-          labelText="measurement"
-        />
-      )}
-      <NumberComponent
-        dataTitle="温度（℃）"
-        value={formValue.temperature}
-        onChange={(value: number) => {
-          setFormValue({
-            ...formValue,
-            temperature: value,
-          });
-        }}
-        labelText="temperature"
-      />
-      <NumberComponent
-        dataTitle="粉量（ｇ）"
-        value={formValue.coffeeAmount}
-        onChange={(value: number) => {
-          setFormValue({
-            ...formValue,
-            coffeeAmount: value,
-          });
-        }}
-        labelText="coffeeAmount"
-      />
-      <NumberComponent
-        dataTitle="湯量（ｇ）"
-        value={formValue.waterAmount}
-        onChange={(value: number) => {
-          setFormValue({
-            ...formValue,
-            waterAmount: value,
-          });
-        }}
-        labelText="waterAmount"
-      />
-      {extractionMethod === "水出し" ? (
-        <HourComponent
-          dataTitle="水出し抽出時間（時間）"
-          onChange={(value: number) =>
-            setFormValue({ ...formValue, extractionTime: value })
-          }
-          value={formValue.extractionTime}
-          labelText="extractionTime"
-        />
-      ) : (
-        <MinuteSecondComponent
-          dataTitle="抽出時間"
-          onChange={(value: number) =>
-            setFormValue({ ...formValue, extractionTime: value })
-          }
-          value={formValue.extractionTime}
-          labelText="extractionTime"
-        />
-      )}
+      <CoffeeBeansComponent />
+      <BrewingRecipeComponent />
       <TastingEvaluationComponent />
-      <TextAreaComponent
-        dataTitle="メモ"
-        onChange={(value: string) => {
-          setFormValue({ ...formValue, memo: value });
-        }}
-        value={formValue.memo}
-        labelText="memo"
-      />
+      <MemoAreaComponent />
     </div>
   );
 };
