14395971632190ab1841bf5eb74b0e9fa6e06c34
akiran1sei <nakamori.1224@gmail.com>
2025-08-19 10:28:40 +0900

    作成したボタンのコンポーネントの確認するための出力ページ

--- app/pages/list/page.tsx
+++ app/pages/list/page.tsx
@@ -1,94 +1,14 @@
-import { MainButton, MainLinkButton } from "@/app/components/buttons/page";
+import ButtonView from "@/app/components/buttons/view/page";
 import { PageTitle } from "@/app/components/title/page";
 
 import styles from "@/app/styles/Pages.module.css";
 const ListPage = () => {
-  const value = `/list`;
   return (
     <div className={styles.listPageContents}>
       <h1>
         <PageTitle value="List Page" />
       </h1>
-      <MainButton
-        sizeValue="large"
-        textValue="削除"
-        buttonColor="btn-danger"
-        widthValue="widthAuto;"
-      />
-      <MainButton
-        sizeValue="middle"
-        textValue="削除"
-        buttonColor="btn-danger"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="large"
-        textValue="PDFをダウンロード"
-        buttonColor="btn-main"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="large"
-        textValue="Switch to Self Ver."
-        buttonColor="btn-success"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="large"
-        textValue="Switch to Shop Ver."
-        buttonColor="btn-success"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="large"
-        textValue="編集"
-        buttonColor="btn-warning"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="large"
-        textValue="保存"
-        buttonColor="btn-success"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="middle"
-        textValue="リセット"
-        buttonColor="btn-secondary"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="middle"
-        textValue="セルフVer"
-        buttonColor="btn-success"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="middle"
-        textValue="お店Ver"
-        buttonColor="btn-success"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="small"
-        textValue="並び替え"
-        buttonColor="btn-secondary"
-        widthValue="widthAuto"
-      />
-      <MainButton
-        sizeValue="small"
-        textValue="検索"
-        buttonColor="btn-secondary"
-        widthValue="widthAuto"
-      />
-
-      <MainLinkButton
-        sizeValue="large"
-        textValue="編集"
-        buttonColor="btn-warning"
-        urlValue={`/pages/${value}`}
-        widthValue="widthAuto"
-      />
+      <ButtonView />
     </div>
   );
 };
